92892d7cfe53ca9a486f917141c61b041e157e47
ClementChaumel <clement.chaumel@gmail.com>
2021-06-03 00:19:52 +0200

    💄 Style fenetres

--- src/components/mood.js
+++ src/components/mood.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel as ReactCarousel } from "react-responsive-carousel";
+import fenetreDefault from "../images/Fenetres/fenetre_vide.png";
 
 const Mood = ({ title, type = "horizontal", src }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,16 +18,15 @@ const Mood = ({ title, type = "horizontal", src }) => {
   const Wrapper = typeClassMap[type];
 
   return (
-    <div className="bg-white border-4 border-black col-span-3">
-      <div className="flex items-center">
-        <button
-          className="bg-white px-4"
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
-          -
-        </button>
+    <div className="bg-white col-span-3">
+      <div
+        className="flex items-center fenetre"
+        style={{ backgroundImage: `url(${fenetreDefault})` }}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <button className={`button ${isOpen ? "button--open" : ""}`} />
         <h3 className="text-lg">{title}</h3>
       </div>
       <Wrapper isOpen={isOpen} src={src} numberOfPanel />
@@ -44,6 +44,10 @@ const Horizontal = ({ isOpen, src }) => {
         overflow-x-scroll
         transition-height
         duration-1000
+        wrapper
+        border-black
+        border-4
+        ${isOpen ? "border-t-4 border-b-4" : "border-t-0 border-b-0"}
         ease-in-out`}
     >
       <img className="max-w-none h-96 pb-3.5" src={src[0]} />
@@ -58,13 +62,17 @@ const Comic = ({ isOpen, src }) => {
         overflow-hidden
         transition-height
         duration-1000
+        wrapper
+        border-black
+        border-4
+        ${isOpen ? "border-t-4 border-b-4" : "border-t-0 border-b-0"}
         ease-in-out`}
-      style={{ height: isOpen ? "291px" : "0px" }}
+      style={{ height: isOpen ? "305px" : "0px" }}
     >
       <img
         className="max-w-none pb-3.5"
         src={src[0]}
-        style={{ height: "305px" }}
+        style={{ height: "311px" }}
       />
     </div>
   );
@@ -75,9 +83,13 @@ const Carousel = ({ isOpen, src }) => {
     <div
       className={`${isOpen ? "max-h-96" : "max-h-0"} 
         overflow-y-hidden
-        overflow-x-scroll
+        overflow-x-hidden
         transition-max-height
         duration-1000
+        wrapper
+        border-black
+        border-4
+        ${isOpen ? "border-t-4 border-b-4" : "border-t-0 border-b-0"}
         ease-in-out`}
     >
       <ReactCarousel
@@ -101,6 +113,10 @@ const Vertical = ({ isOpen, src }) => {
       className={`
         overflow-y-scroll
         transition-max-height
+        wrapper
+        border-black
+        border-4
+        ${isOpen ? "border-t-4 border-b-4" : "border-t-0 border-b-0"}
         ease-in`}
       style={{
         maxHeight: isOpen ? "80vh" : "0px",
@@ -113,7 +129,7 @@ const Vertical = ({ isOpen, src }) => {
 };
 
 Mood.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   src: PropTypes.array.isRequired,
   type: PropTypes.oneOf(["horizontal", "comic", "carousel", "vertical"]),
   numberOfPanel: PropTypes.number,
